4fddb1af8e3119a22ca3fefa154203c10a373f4f
AsmitX <162042032+AsmitX@users.noreply.github.com>
2024-05-12 21:16:49 +0545

    Add files via upload

---
+++ dump_enc.py
@@ -0,0 +1,8 @@
+#__________________| INFO |___________________#
+#______SCRIPT ENCRYPTED BY PYTHON 3.0
+#______CODING BY: ASM1T 1N
+#______FACEBOOK : https://www.facebook.com/Asmit021
+#________________| SCRIPT DATA |_____________#
+
+import marshal,zlib,base64
+exec(marshal.loads(zlib.decompress(base64.b64decode(b'eJx7zIAEGKH0ZzYgMZ0hhSGYQZPRbyVDERNIECRziykl/ReHTW5+SmlOql0RK1RTMT+Q+MDMyMj4gQFGFDEDxQC83A+o'))))
